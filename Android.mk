de1e308b335193a3a0207de87f1a544e1e6ec903
Ksawlii <maja.blaszczak@proton.me>
2025-08-10 22:18:07 +0200

    a25x: Init
    Signed-off-by: Ksawlii <maja.blaszczak@proton.me>

---
+++ Android.mk
@@ -0,0 +1,23 @@
+#
+# Automatically generated file. DO NOT MODIFY
+#
+
+LOCAL_PATH := $(call my-dir)
+
+ifeq ($(TARGET_DEVICE),a25x)
+
+$(call add-radio-file-sha1-checked,radio/fld.bin_SM-A256E,df9655a0d4f03d3db2d473e4ff9ea5e8ba913267)
+$(call add-radio-file-sha1-checked,radio/harx.bin_SM-A256E,614ea243a0bec95aa1ceb47ebb2c9baf0c73fb17)
+$(call add-radio-file-sha1-checked,radio/keystorage.bin_SM-A256E,a8f83d2f3e6bfb718b102efcfe585e763b7afd8e)
+$(call add-radio-file-sha1-checked,radio/ldfw.img_SM-A256E,27ad7e807478eed8086a2f1ef8a678c666ad0a21)
+$(call add-radio-file-sha1-checked,radio/models,f78b3d83dc19645a8cd7abd00aad149ef6feb5b8)
+$(call add-radio-file-sha1-checked,radio/modem.bin_SM-A256E,04ace5dda3c10bfd96d4ce4b3c0d1e0625fd5906)
+$(call add-radio-file-sha1-checked,radio/modem_debug.bin_SM-A256E,b3f09946760b09abe2ef14b14ab8c037a22a3b2c)
+$(call add-radio-file-sha1-checked,radio/sboot.bin_SM-A256E,caa6967b51d7b50a2e90eed48f723c53f069bfc4)
+$(call add-radio-file-sha1-checked,radio/tzar.img_SM-A256E,42f2656d46b16cfdc3d6e30079b44d3f44e44e90)
+$(call add-radio-file-sha1-checked,radio/tzsw.img_SM-A256E,5e399dbc71beb8f3c90964d2b0dc68ba88214bcc)
+$(call add-radio-file-sha1-checked,radio/uh.bin_SM-A256E,0182a122d0deb6aefb53539b48e35a9575b56425)
+$(call add-radio-file-sha1-checked,radio/up_param.bin_SM-A256E,5b32fa1d92c5f8b95e16ca20d36a2a565533b1df)
+$(call add-radio-file-sha1-checked,radio/version_SM-A256E,10c4329c8ffbb136128ab6c005ca86cd2a8dda59)
+
+endif
