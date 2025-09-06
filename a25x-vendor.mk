381b0c39b3f4366867c4ac5fc8e33dfbdcd809a7
Majaahh <maja.blaszczak@proton.me>
2025-09-06 21:07:59 +0200

    a25x: Drop 32 Bit blobs
    Change-Id: Iff6e3bb4391a15104769edd0f7a99bd4a3820f6b
    Signed-off-by: Majaahh <maja.blaszczak@proton.me>

--- a25x-vendor.mk
+++ a25x-vendor.mk
@@ -195,7 +195,6 @@ PRODUCT_COPY_FILES += \
     vendor/samsung/a25x/proprietary/vendor_ramdisk/vendor/firmware/gt9895_a25x.bin:$(TARGET_COPY_OUT_VENDOR_RAMDISK)/vendor/firmware/gt9895_a25x.bin
 
 PRODUCT_PACKAGES += \
-    libhermes \
     android.weaver@1.0 \
     camera.s5e8825 \
     libMERTA \
@@ -205,6 +204,7 @@ PRODUCT_PACKAGES += \
     libexynoscamera_merta_plugin \
     libexynoscamera_swlme_plugin \
     libexynoscamera_vpl_plugin \
+    libhermes \
     libhermes_bdbridge \
     libhwvault \
     libisosechw \
